a789fb64b84add830a2f784a33bbc92142495e17
nafsan1 <mnafsan@gmail.com>
2019-05-17 15:24:37 +0700

    implementasi content provider

---
+++ MyFavouriteMovie/app/src/main/java/com/example/myfavouritemovie/model/TvDataResponse.java
@@ -0,0 +1,23 @@
+package com.example.myfavouritemovie.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class TvDataResponse {
+    @SerializedName("results")
+    @Expose
+    private List<Tv> tv = null;
+
+    public TvDataResponse(List<Tv> tv ){
+        this.tv = tv;
+    }
+    public List<Tv> getTv() {
+        return tv;
+    }
+
+    public void setTv(List<Tv> tv) {
+        this.tv = tv;
+    }
+}
